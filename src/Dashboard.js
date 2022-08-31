84bcec4cff4f1b306c166f6fb28e8d7887bf964d
Shubham Sutar <shubham@dronapay.com>
2022-08-31 20:46:42 +0530

    update ui

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+import Main from "./Main";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
 const Dashboard = (props) => {
   const history = useNavigate();
   const [user, setUser] = useState("")
+  const [title1, setTitle1] = useState("Featured title2")
+
+  const getData = () => {
+    var t1 = prompt('Enter Title1');
+    setTitle1(t1)
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("rest-music-site");
@@ -25,6 +33,8 @@ const Dashboard = (props) => {
   return (
     <div className="container">
       <Navbar isAuth={props.isAuth} user={user} history={history} />
+      <Main title1={title1} getData={getData} />
+      <Footer isAuth={props.isAuth} user={user} history={history} />
     </div>
   );
 };
---
+++ src/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="footer mt-auto py-3 bg-light mt-auto">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
