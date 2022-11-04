9077d6497ebd482a0e602f50b462e82bb3715a84
Shubham Sutar <shubham@dronapay.com>
2022-11-04 21:58:51 +0530

    navbar and pages code refactor

--- src/Screens/Dashboard.js
+++ src/Screens/Dashboard.js
@@ -1,15 +1,10 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../layout/Navbar";
-import { useNavigate } from "react-router-dom";
 
-const Dashboard = (props) => {
-  const history = useNavigate();
-  const [user, setUser] = useState("")
-  
+const Dashboard = () => {
   return (
     <div className="container-fluid px-0">
-      <Navbar isAuth={props.isAuth} user={user} history={history} />
+      <Navbar />
     </div>
   );
 };
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,9 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 import { Link } from "react-router-dom";
 import { BsFillPersonFill } from "react-icons/bs";
 
 const Navbar = (props) => {
+  const [user, setUser] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("rest-music-site");
+    if (token) {
+      var decoded = jwt_decode(token);
+      // console.log(decoded);
+      setUser(decoded.firstName);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <nav className="navbar navbar-expand-lg navbar-light navbar_bg_color">
       <div className="container-fluid">
@@ -34,19 +46,19 @@ const Navbar = (props) => {
               </Link>
             </li>
           </ul>
-          {props.isAuth ? (
-            <form className="d-flex ms-5">
-              <p className="mb-0 mx-2 mt-1">
-                <BsFillPersonFill className="mx-1" />
-                {props.user}
-              </p>
-              <Link to="/logout" className="btn btn-outline-info text-dark btn-sm">
-                Log Out
-              </Link>
-            </form>
-          ) : (
-            props.history("/")
-          )}
+
+          <form className="d-flex ms-5">
+            <p className="mb-0 mx-2 mt-1">
+              <BsFillPersonFill className="mx-1" />
+              {user}
+            </p>
+            <Link
+              to="/logout"
+              className="btn btn-outline-info text-dark btn-sm"
+            >
+              Log Out
+            </Link>
+          </form>
         </div>
       </div>
     </nav>
