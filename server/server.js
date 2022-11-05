ffe4dee6da68186a667a64acb0623656b58563ea
Shubham Sutar <shubham@dronapay.com>
2022-11-05 12:08:17 +0530

    route created at backend

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
 var app = express();
-const {User, validateRegister, validateLogin} = require('./model/user');
-const bcrypt = require('bcrypt');
 
 const connection = require('./db');
 
@@ -23,71 +21,9 @@ app.get('/', (req, res) => {
   res.send(`Example app listening on port http://localhost:${port}`)
 })
 
-// const authRouter = require('./routes/authRouter');
+const authRouter = require('./routes/authRouter');
 
-// app.use("/auth", authRouter);
-
-app.post('/auth/register', async (req, res) => {
-  // console.log(req.body);
-  try {
-    const {error} = validateRegister(req.body);
-    // console.log(error);
-    if (error) {
-        return res.send({errorMsg: error.details[0].message});
-    }
-
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
-
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-    // console.log(hashPassword);
-    const result = await new User({...req.body, password: hashPassword}).save();
-    console.log(result);
-
-    res.send({successMsg: "User Registered successfully"});
-  } catch (error) {
-    res.status(500).send({ errorMsg: error });
-  }
-});
-
-app.post('/auth/login', async (req, res) => {
-  // console.log(req.body);
-  try {
-    const {error} = validateLogin(req.body);
-    if (error) {
-        return res.send({errorMsg: error.details[0].message});
-    }
-
-    const user = await User.findOne({email: req.body.email});
-    if (!user) {
-        return res.send({errorMsg: "Invalid Email or Password"})
-    }
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) {
-        return res.send({errorMsg: "Invalid Email or Password"})
-    }
-
-    const token = user.generateAuthToken();
-    res.status(200).send({data:token, successMsg: "Logged in successfully"});
-  } catch (error) {
-    res.send({ errorMsg: error });
-  }
-});
-
-app.post('/auth/verify-url', async (req, res) => {
-  // console.log(req.body);
-  try {
-
-    const user = await User.findOne({email: req.body.email});
-    if (!user) {
-        return res.send({errorMsg: "User Not Found"})
-    }
-
-    res.status(200).send({successMsg: "User Found"});
-  } catch (error) {
-    res.send({ errorMsg: error });
-  }
-});
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
