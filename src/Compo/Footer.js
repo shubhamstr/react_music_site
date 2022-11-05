9cc83bf89d83bf45d79f0405682ef5f4ce1cc71d
Shubham Sutar <shubham@dronapay.com>
2022-11-05 09:42:36 +0530

    add footer

---
+++ src/Compo/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer_bg_color d-flex justify-content-center">
+        <h5 className="my-2">Footer</h5>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Compo/Layout.js
+++ src/Compo/Layout.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../Compo/Navbar";
+import Footer from "../Compo/Footer";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className="container-fluid px-0">
       <Navbar />
-      {children}
+      <div className="content">{children}</div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
